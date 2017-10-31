fac9b006d0dc9f36f68cd226a2267c49a9260bd3
Vivify Academy <ladybutterfly.fly@gmail.com>
2017-10-31 19:23:31 +0100

    uredjivanje home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	selectedItem: any;
+		items: Array<{title: string, note: string}>;
+	  constructor (public navCtrl: NavController, public navParams: NavParams) {
 
+			this.selectedItem = navParams.get('item');
+
+			this.items = [];
+			for (let i = 1; i <= 10; i++) {
+				this.items.push({
+					title: 'Item ' + i,
+					note: 'This is item #' + i
+				});
+			}
   }
 
 }
