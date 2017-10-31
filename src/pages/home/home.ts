547fb5fa713a5aa4b87a06fd72c75efba284197a
Vivify Academy <ladybutterfly.fly@gmail.com>
2017-10-31 20:39:15 +0100

    icons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,16 +8,24 @@ import { NavController, NavParams } from 'ionic-angular';
 export class HomePage {
 
 	selectedItem: any;
-		items: Array<{title: string, note: string}>;
+		items: Array<{title: string, note: string, icon: string}>;
+		icons: string[];
+
 	  constructor (public navCtrl: NavController, public navParams: NavParams) {
 
 			this.selectedItem = navParams.get('item');
+
+			this.icons = [
+				'flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+				'american-football', 'boat', 'bluetooth', 'build'
+			];
 
 			this.items = [];
 			for (let i = 1; i <= 10; i++) {
 				this.items.push({
 					title: 'Item ' + i,
-					note: 'This is item #' + i
+					note: 'This is item #' + i,
+					icon: this.icons[i - 1]
 				});
 			}
   }
